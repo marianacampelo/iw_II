576ce06a320598b57598e47542fbfcccc3c848d2
Mariana Campello <125597199+marianacampelo@users.noreply.github.com>
2024-02-19 16:55:42 -0300

    Add files via upload

---
+++ aula_01/code.js
@@ -0,0 +1,15 @@
+function cnome() {
+    let n = document.getElementById(nome).value 
+    alert("Bem vindo ao JS: " + n)
+
+}
+
+
+
+function ola(){
+    console.log("Olá JS2")
+    alert("Meu Front-End")
+}
+
+ola()
+
